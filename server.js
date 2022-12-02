ff1dd218f0c9dce9a33738f606b500e12b33943d
Vinícius Souza do Espírito Santo <86697108+vinicius-vses@users.noreply.github.com>
2022-12-02 12:38:58 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const { response } = require("express");
-const express = require("express");
-const app = express();
-
-app.listen(porta = 3000, () => {
-    console.log("Servidor executando na porta "
-        + porta);
-});
